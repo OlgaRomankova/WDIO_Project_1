10a6e77df9b71ca7c9d95d10e0e94ccb3cd5b231
OlgaRomankova <olcha777@tut.by>
2024-11-03 23:07:09 +0300

    Click to Google search box

--- test/specs/test1.spec.js
+++ test/specs/test1.spec.js
@@ -1,5 +1,8 @@
 describe('Demo Tests', function () {
     it('My First Test', async () => {
       await browser.url('https://google.com');
+      $('[name="q"]').setValue('WebdriverIO');
+       $('[name="btnK"]').click();
+      browser.pause(2000)
     });
 });
