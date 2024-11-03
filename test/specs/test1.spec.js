6acb34175d8e0c3e572fa24e772a691cd609e5f9
OlgaRomankova <olcha777@tut.by>
2024-11-03 23:32:49 +0300

    Enter WebdriverIO to Search box and hit Enter

--- test/specs/test1.spec.js
+++ test/specs/test1.spec.js
@@ -1,8 +1,16 @@
 describe('Demo Tests', function () {
-    it('My First Test', async () => {
-      await browser.url('https://google.com');
-      $('[name="q"]').setValue('WebdriverIO');
-       $('[name="btnK"]').click();
-      browser.pause(2000)
-    });
+  it('My First Test', async () => {
+    await browser.url('https://google.com');
+    browser.pause(2000);
+    await $('[name="q"]').setValue('WebdriverIO');
+    await $('input[name="btnK"]').click();
+   
+    // await browser.waitUntil(
+    //   async () => {
+    //     return await $('input[name="btnK"]').isDisplayed(); // Ждем, пока кнопка станет доступной
+    //   },
+    //   { timeout: 2000, timeoutMsg: 'Кнопка поиска не доступна' }
+    // );
+    // await $('input[name="btnK"]').click();
+  });
 });
